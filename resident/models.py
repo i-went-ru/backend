5ed0b6ce8b298a8451b4a5e9818dc868457f2c08
toastmanager <mrbonkyou@gmail.com>
2023-11-22 08:31:23 +0900

    add_photo action

--- resident/models.py
+++ resident/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from rest_framework.exceptions import bad_request
 
 User = get_user_model()
 
@@ -15,6 +16,18 @@ class Resident(models.Model):
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=2500)
     direction = models.CharField(max_length=17, choices=DIRECTIONS)
+
+    def add_photo(self, user, image):
+        if user != self.responsible:
+            return bad_request
+        if not image:
+            return bad_request
+        
+        new_photo = ResidentPhotos()
+        new_photo.resident = self
+        new_photo.photo = image
+        new_photo.save()
+        
 
     def __str__(self) -> str:
         return f'{self.pk} | {self.name} | {self.direction}'
--- resident/views.py
+++ resident/views.py
@@ -1,8 +1,17 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Resident
-from .serializers import ResidentSerializer
+from .serializers import ResidentSerializer, ResidentPhotoSerializer
 
 class ResidentViewSet(viewsets.ModelViewSet):
     queryset = Resident.objects.all()
     serializer_class = ResidentSerializer
+
+    @action(detail=True, methods=['post'], permission_classes=[IsAuthenticated], serializer_class=ResidentPhotoSerializer)
+    def add_photo(self, request, pk=None):
+        resident: Resident = self.get_object()
+        resident.add_photo(request.user, request.data['photo'])
+        return Response('successfully added a new photo')
