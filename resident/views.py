089c35868c684698e3420e92b3221cafee59406f
toastmanager <mrbonkyou@gmail.com>
2023-11-22 23:50:51 +0900

    fixed residents tags

--- resident/views.py
+++ resident/views.py
@@ -1,7 +1,10 @@
+from typing import override
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 from .models import Resident
 from .serializers import ResidentSerializer, ResidentPhotoSerializer
@@ -10,10 +13,26 @@ class ResidentViewSet(viewsets.ModelViewSet):
     queryset = Resident.objects.all()
     serializer_class = ResidentSerializer
 
+    
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter(
+                name='tags',
+                in_=openapi.IN_QUERY,
+                type=openapi.TYPE_STRING,
+                description='Comma-separated list of tags',
+                required=False,
+            ),
+        ],
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
     def get_queryset(self):
         queryset = super().get_queryset()
-        tags = self.request.query_params.get('tags', '').split(',')
-        if tags:
+        tags_string = self.request.query_params.get('tags', '')
+        if tags_string:
+            tags = tags_string.split(',')
             queryset = queryset.filter(tags__name__in=tags)
         return queryset
 
