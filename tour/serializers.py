d19cac081696cff11d214198b79ffe9d40a2c27d
toastmanager <mrbonkyou@gmail.com>
2023-11-22 11:12:54 +0900

    info about users in tour

--- tour/serializers.py
+++ tour/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 
 from .models import Tour
+from user.serializers import UserSerializerFull
 
 class TourSerializer(serializers.ModelSerializer):
+    guide = UserSerializerFull()
+    client = UserSerializerFull()
     class Meta:
         model = Tour
         fields = ('guide', 'client', 'datetime', 'status')
