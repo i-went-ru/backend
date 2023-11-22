0b2f59d2a7ebba461a80978e3ba38a3816c53fce
toastmanager <mrbonkyou@gmail.com>
2023-11-22 12:20:43 +0900

    list of residents in tours

--- tour/models.py
+++ tour/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from resident.models import Resident
 
 User = get_user_model()
 
@@ -11,10 +12,11 @@ class Tour(models.Model):
         ('notapproved', 'не одобрен'),
     )
     guide = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name='guided_tours')
-    client = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name='tours')
+    client = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name='client_tours')
     datetime = models.DateTimeField(unique=True)
     status = models.CharField(max_length=11, choices=STATUSES)
     comment = models.CharField(max_length=2500)
+    residents = models.ManyToManyField(Resident, related_name='resident_tours')
 
     def __str__(self) -> str:
         return f'{self.pk} | {self.datetime} | {self.status}'
--- tour/serializers.py
+++ tour/serializers.py
@@ -2,10 +2,13 @@ from rest_framework import serializers
 
 from .models import Tour
 from user.serializers import UserSerializerFull
+from resident.serializers import ResidentSerializer
 
 class TourSerializer(serializers.ModelSerializer):
     guide = UserSerializerFull()
     client = UserSerializerFull()
+    residents = ResidentSerializer(many=True)
+
     class Meta:
         model = Tour
-        fields = ('guide', 'client', 'datetime', 'status')
+        fields = ('id', 'datetime', 'status', 'guide', 'client', 'residents')
