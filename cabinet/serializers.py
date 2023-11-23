ea7463b4ee76e13fdcb06643889fcf456b5ce623
toastmanager <mrbonkyou@gmail.com>
2023-11-23 23:29:07 +0900

    resident info now read only

--- cabinet/serializers.py
+++ cabinet/serializers.py
@@ -4,7 +4,7 @@ from .models import Cabinet
 from resident.serializers import ResidentSerializer
 
 class CabinetSerializer(serializers.ModelSerializer):
-    resident_info = ResidentSerializer(source='resident')
+    resident_info = ResidentSerializer(source='resident', read_only=True)
     class Meta:
         model = Cabinet
         fields = ('id', 'resident', 'resident_info', 'cabinet_number', 'color', 'path')
