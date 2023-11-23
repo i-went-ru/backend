f09b7de1ab7d240fb2b60657a83c84c43c0c6037
toastmanager <mrbonkyou@gmail.com>
2023-11-23 23:16:41 +0900

    responsible info in resident serializer

--- resident/serializers.py
+++ resident/serializers.py
@@ -1,7 +1,10 @@
+from django.contrib.auth import get_user_model
+
 from rest_framework import serializers
 from taggit.serializers import TagListSerializerField, TaggitSerializer
 
 from .models import Resident, ResidentPhotos, FreeDay, BusyDay, ExtraFile
+from user.serializers import UserSerializerFull
 
 class ResidentPhotoSerializer(serializers.ModelSerializer):
     class Meta:
@@ -23,13 +26,19 @@ class ExtraFilesSerializer(serializers.ModelSerializer):
         model = ExtraFile
         fields = ('file',)
 
+class ResponsibleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ('full_name', 'organization', 'phone')
+
 class ResidentSerializer(TaggitSerializer, serializers.ModelSerializer):
     photos = ResidentPhotoSerializer(many=True, read_only=True)
     tags = TagListSerializerField()
     free_days = BusyDaySerializer(many=True, read_only=False, required=False)
     busy_days = FreeDaySerializer(many=True, read_only=False, required=False)
     extra_files = ExtraFilesSerializer(many=True, read_only=True)
+    responsible_info = ResponsibleSerializer(source='responsible')
 
     class Meta:
         model = Resident
-        fields = ('id', 'responsible', 'name', 'description', 'direction', 'floor', 'photos', 'tags', 'busy_days', 'free_days', 'extra_files')
+        fields = ('id', 'responsible', 'responsible_info', 'name', 'description', 'direction', 'floor', 'photos', 'tags', 'busy_days', 'free_days', 'extra_files')
--- tour/models.py
+++ tour/models.py
@@ -12,6 +12,7 @@ class Tour(models.Model):
         ('notapproved', 'не одобрен'),
         ('sended', 'отправлено'),
     )
+
     guide = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name='guided_tours')
     client = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name='client_tours')
     guest_count = models.IntegerField(blank=False)
@@ -21,11 +22,41 @@ class Tour(models.Model):
     comment = models.CharField(max_length=2500, blank=True, null=True)
     residents = models.ManyToManyField(Resident, related_name='resident_tours', blank=True)
 
+    # properties
     @property
     def importance(self):
         if self.client.user_type == 'org': # type: ignore
             return 'high'
         return 'medium'
+    
+    # actions
+    def change_begin_datetime(self, datetime):
+        # TODO
+        pass
+    
+    def change_end_datetime(self, datetime):
+        # TODO
+        pass
+
+    def change_guests_count(self, amount):
+        # TODO
+        pass
+    
+    def change_guide(self, user):
+        # TODO
+        pass
+    
+    def change_status(self, status):
+        # TODO
+        pass
+    
+    def add_resident(self, resident):
+        # TODO
+        pass
+    
+    def delete_resident(self, resident):
+        # TODO
+        pass
 
     def __str__(self) -> str:
         return f'{self.pk} | {self.begin_datetime} — {self.end_datetime} | {self.status}'
