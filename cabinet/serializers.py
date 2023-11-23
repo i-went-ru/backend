da3c5f1cef4befc8ae849a7d51fbe58843586adc
toastmanager <mrbonkyou@gmail.com>
2023-11-23 23:22:39 +0900

    resident info in cabinet serializer

--- cabinet/serializers.py
+++ cabinet/serializers.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
 
 from .models import Cabinet
+from resident.serializers import ResidentSerializer
 
 class CabinetSerializer(serializers.ModelSerializer):
+    resident_info = ResidentSerializer(source='resident')
     class Meta:
         model = Cabinet
-        fields = ('id', 'resident', 'cabinet_number', 'color', 'path')
+        fields = ('id', 'resident', 'resident_info', 'cabinet_number', 'color', 'path')
