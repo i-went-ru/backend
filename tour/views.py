3b383963285b7b8b58c0c53e3165ef81d9ba1cb7
toastmanager <mrbonkyou@gmail.com>
2023-11-24 11:27:38 +0900

    only approved tours in upcoming

--- tour/views.py
+++ tour/views.py
@@ -19,19 +19,19 @@ class TourViewSet(viewsets.ModelViewSet):
 
     @action(detail=False, methods=['get'])
     def upcoming(self, request):
-        queryset = Tour.objects.filter(begin_datetime__gte=datetime.now()).order_by('begin_datetime')
+        queryset = Tour.objects.filter(begin_datetime__gte=datetime.now()).filter(status='approved').order_by('begin_datetime')
         return Response(TourSerializer(queryset, many=True).data)
     
     @action(detail=False, methods=['get'], url_path='upcoming/week')
     def upcoming_week(self, request):
         today = datetime.now().date()
         week_from_now = today + timedelta(weeks=1)
-        queryset = Tour.objects.filter(begin_datetime__range=[today, week_from_now]).order_by('begin_datetime')
+        queryset = Tour.objects.filter(begin_datetime__range=[today, week_from_now]).filter(status='approved').order_by('begin_datetime')
         return Response(TourSerializer(queryset, many=True).data)
     
     @action(detail=False, methods=['get'], url_path='upcoming/month')
     def upcoming_month(self, request):
         today = datetime.now().date()
         month_from_now = today + timedelta(days=30)
-        queryset = Tour.objects.filter(begin_datetime__range=[today, month_from_now]).order_by('begin_datetime')
+        queryset = Tour.objects.filter(begin_datetime__range=[today, month_from_now]).filter(status='approved').order_by('begin_datetime')
         return Response(TourSerializer(queryset, many=True).data)
